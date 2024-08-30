61c81fe1062d1861ff55959d8159fb326c67ac57
Sulaimon Taofeek Ayomide <sulaimontaofeekgithub100@gmail.com>
2024-08-30 18:59:37 +0100

    update on responsive

--- src/components/dashboard/history.jsx
+++ src/components/dashboard/history.jsx
@@ -169,21 +169,6 @@ export default function DashboardInvoice() {
                         </div>
                     </div>
 
-
-                    <footer className="footer">
-                        <div className="container-fluid">
-                            <div className="row">
-                                <div className="col-sm-6">
-                                    <script>document.write(new Date().getFullYear())</script> © Velzon.
-                                </div>
-                                <div className="col-sm-6">
-                                    <div className="text-sm-end d-none d-sm-block">
-                                        Design & Develop by Themesbrand
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </footer>
                 </div>
             </div>
         </>
